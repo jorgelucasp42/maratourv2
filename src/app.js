780e3e84686f45a28a9d48210e44e28ee26f9c9c
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-08-03 10:57:48 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,22 +2,45 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const destinos = [
     {
         id: 1,
-        título: "São Luís"
+        nome: "São Luís"
     },
     {
         id: 2,
-        título: "Atins"
+        nome: "Atins"
     }
 ];
+
+function buscaDestino(id) {
+    return destinos.findIndex(destino => {
+        return destino.id === Number(id);
+    });
+}
 
 app.get("/", (req, res) => {
   res.status(200).send("MaraTour");
 });
+
 app.get("/destinos", (req, res) => {
     res.status(200).json(destinos);
-  });
+});
+
+app.get("/destinos/:id", (req, res) => {
+    const index = buscaDestino(req.params.id);
+    if (index !== -1) {
+        res.status(200).json(destinos[index]);
+    } else {
+        res.status(404).send("Destino não encontrado");
+    }
+});
+
+app.post("/destinos", (req, res) => {
+    destinos.push(req.body);
+    res.status(201).send("Destino cadastrado com sucesso");
+});
 
 export default app;
