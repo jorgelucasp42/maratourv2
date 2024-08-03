a4311c6bb1261aebd9af407e31c4effdf48d8fb0
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-08-03 10:11:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //import http from "http";
 
-import app from "./src/app";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
@@ -10,11 +10,11 @@ const rotas = {
   "/destinos": "Entrei na rota destinos"
 };
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(rotas[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
