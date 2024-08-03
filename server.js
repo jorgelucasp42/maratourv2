2d2f301df9464a9b8fa2a1b51ab91e6c01bbc9f2
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-08-03 10:01:01 -0300

    update

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "MaraTour",
+  "/sobre": "Entrei na rota sobre",
+  "/destinos": "Entrei na rota destinos"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("MaraTour");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
