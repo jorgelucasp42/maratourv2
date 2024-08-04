078a778d312952142b334fb8e5d98fd03b0755f9
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-08-04 17:02:16 -0300

    update

--- src/models/atrativo.js
+++ src/models/atrativo.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const atrativoSchema = new mongoose.Schema({
     nome: { type: String, required: true },
-    tipo: { type: String, required: true },
+    tipo: { type: String, required: true }, // e.g., "praia", "monumento", "museu", etc.
     descricao: { type: String, required: true },
     dicas: { type: String },
     imagem: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    destino: { type: mongoose.Schema.Types.ObjectId, ref: 'Destino', required: true }
+    localizacao: {
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true }
+    },
+    destino: { type: mongoose.Schema.Types.ObjectId, ref: 'Destino', required: true },
+    slug: { type: String, required: true, unique: true }
 });
 
 const Atrativo = mongoose.model('Atrativo', atrativoSchema);
